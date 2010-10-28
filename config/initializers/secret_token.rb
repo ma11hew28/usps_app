80825d5363348545f337984de31dec2df66fd2fd
Matt Di Pasquale <mattd@fynanz.com>
2010-10-28 15:25:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UspsApp::Application.config.secret_token = 'b16b8087f2936dc82becd5f7776911eb75023204a256ada753e678c49296507c5bd8b4935ed9a4c62c37151c02ee9f8d28aa56ddf2e5883dd502e7d31fc3da51'
